f74136a513f5cd2a5957a8b667a9fc9d849d9c8a
rajadoui <mehdijadoui22@gmail.com>
2024-11-26 09:41:53 +0100

    Add commentary on Contact file

--- Contact.cs
+++ Contact.cs
@@ -1,9 +1,12 @@
+
 public class Contact
 {
+    // Properties
     public string Name { get; set; }
     public string Email { get; set; }
     public string Phone { get; set; }
 
+    // Override the ToString method
     public override string ToString()
     {
         return $"Name: {Name}, Email: {Email}, Phone: {Phone}";
